0da875e2e1d207e0d44b37aab129b9ac4bad0a6b
tomasshiao <tomasshiao2001@gmail.com>
2019-08-26 10:22:40 -0300

    Mi primer commit

---
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -0,0 +1,23 @@
+package com.codeoftheweb.salvo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SalvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(PlayerRepository playerRepository){
+		return(arg) -> {
+
+			Player player1 = new Player("j.bauer@ctu.gov");
+			playerRepository.save(player1);
+		};
+	}
+}
